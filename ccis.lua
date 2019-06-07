bfcdefdd9d929b97b141abc55f4f5df27d4dd53c
ioniffer <49140856+ioniffer@users.noreply.github.com>
2019-06-07 15:32:51 +0400

    Update ccis.lua

--- ccis.lua
+++ ccis.lua
@@ -17,52 +17,46 @@ function ccis_protocol.dissector(buffer, pinfo, tree)
     -- subtree:add(message_length, buffer(11,2))
   
     if length>26 then
-      
         local from_ip = bytes_to_ip(buffer,26)
-	    local from_port = buffer(42,2):uint()
-	    local FromSubtree = subtree:add(ccis_protocol, buffer(), "From "..from_ip..":"..from_port)
-        --FromSubtree:add("IP " .. from_ip, buffer(26,4))
-	    --FromSubtree:add("Port: " .. from_port, buffer(42,2))
+	local from_port = buffer(42,2):uint()
+	local FromSubtree = subtree:add(ccis_protocol, buffer(), "From "..from_ip..":"..from_port)
+        -- FromSubtree:add("IP " .. from_ip, buffer(26,4))
+	-- FromSubtree:add("Port: " .. from_port, buffer(42,2))
 	  
 	  
-	    local to_ip = bytes_to_ip(buffer,30)
-	    local to_port = buffer(44,2):uint()
-	    local ToSubtree = subtree:add(ccis_protocol, buffer(), "To "..to_ip..":"..to_port)
-	    --ToSubtree:add("IP " .. to_ip, buffer(30,4))
-	    --ToSubtree:add("Port: " .. to_port, buffer(44,2))
+	local to_ip = bytes_to_ip(buffer,30)
+	local to_port = buffer(44,2):uint()
+	local ToSubtree = subtree:add(ccis_protocol, buffer(), "To "..to_ip..":"..to_port)
+	-- ToSubtree:add("IP " .. to_ip, buffer(30,4))
+	-- ToSubtree:add("Port: " .. to_port, buffer(44,2))
 	  
-	    --udp_ip_local = bytes_to_ip(buffer,48)
-	    --udp_ip_remote = bytes_to_ip(buffer,62)
-	    -- subtree:add_le("UDP IP Local: " .. udp_ip_local)
-	    -- subtree:add_le("UDP IP Remote: " .. udp_ip_remote)
-	  
-	    local opcode_number = buffer(94,1):le_uint()
+	-- udp_ip_local = bytes_to_ip(buffer,48)
+	-- udp_ip_remote = bytes_to_ip(buffer,62)
+	-- subtree:add_le("UDP IP Local: " .. udp_ip_local)
+	-- subtree:add_le("UDP IP Remote: " .. udp_ip_remote)
+	local opcode_number = buffer(94,1):le_uint()
         local opcode_name = get_opcode_name(opcode_number)
         pinfo.cols.info = opcode_name
-	    subtree:add_le(opcode_number,         buffer(12,4)):append_text(" " .. opcode_name .. "")
-	    local opc = buffer(96,1)
-	    subtree:add_le("OPC " .. opc)
-	  	  
-	    if opcode_name == "INVITE" then
-	      
-	        local number_b_length = tonumber("0x"..(tostring(buffer(104,1)):sub(0,1)))
-		    -- subtree:add("Number B len: " .. number_b_length,buffer(104,1))
+	subtree:add_le(opcode_number,         buffer(12,4)):append_text(" " .. opcode_name .. "")
+	local opc = buffer(96,1)
+	subtree:add_le("OPC " .. opc)
+	if opcode_name == "INVITE" then
+	    local number_b_length = tonumber("0x"..(tostring(buffer(104,1)):sub(0,1)))
+	    -- subtree:add("Number B len: " .. number_b_length,buffer(104,1))
 		  
-		    local bytes_to_read = math.floor((number_b_length+1)/2)
-		    -- subtree:add("bytes_to_read: " .. bytes_to_read)
+            local bytes_to_read = math.floor((number_b_length+1)/2)
+            -- subtree:add("bytes_to_read: " .. bytes_to_read)
 		  
-		    local number_b_reversed = buffer(105,bytes_to_read)
-		    local number_b = byte_reverse(number_b_reversed):sub(0,number_b_length)
-		    subtree:add("Number B: " .. number_b,buffer(105,bytes_to_read))
+            local number_b_reversed = buffer(105,bytes_to_read)
+            local number_b = byte_reverse(number_b_reversed):sub(0,number_b_length)
+            subtree:add("Number B: " .. number_b,buffer(105,bytes_to_read))
 		  
-		    local number_a_reversed = buffer(109+bytes_to_read,2)
-		    local number_a = (byte_reverse(number_a_reversed)):gsub("a", "0")
-		    subtree:add("Number A: " .. number_a)
+            local number_a_reversed = buffer(109+bytes_to_read,2)
+            local number_a = (byte_reverse(number_a_reversed)):gsub("a", "0")
+            subtree:add("Number A: " .. number_a)
         end
-  end
-  
+    end
     --subtree:add(ip_address,buffer(159,4))
-  
 end
 
 function byte_reverse(reversed)
@@ -84,16 +78,16 @@ function bytes_to_ip(buffer,start)
 end
 
 function get_opcode_name(opcode)
-  local opcode_name = "Unknown"
+    local opcode_name = "Unknown"
 
-      if opcode == 63 then opcode_name = "INVITE"
-  elseif opcode ==  8 then opcode_name = "OK"
-  elseif opcode ==  7 then opcode_name = "BYE"
-  elseif opcode == 26 then opcode_name = "DESCRIPTION 1"
-  elseif opcode == 27 then opcode_name = "DESCRIPTION 2"
-      end
+        if opcode == 63 then opcode_name = "INVITE"
+    elseif opcode ==  8 then opcode_name = "OK"
+    elseif opcode ==  7 then opcode_name = "BYE"
+    elseif opcode == 26 then opcode_name = "DESCRIPTION 1"
+    elseif opcode == 27 then opcode_name = "DESCRIPTION 2"
+        end
 
-  return opcode_name
+    return opcode_name
 end
 
 
