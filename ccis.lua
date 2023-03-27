162ff0f6ffe7b24fd88d89298097761f99625be9
Mahir GÜL <mahirgul@gmail.com>
2023-03-28 00:38:28 +0200

    Update ccis.lua

--- ccis.lua
+++ ccis.lua
@@ -4,6 +4,7 @@ message_length = ProtoField.int32("ccis.message_length", "messageLength", base.D
 from_ip = ProtoField.int32("ccis.from_ip","from_ip", base.DEC)
 from_port = ProtoField.int32("ccis.from_port","from_port", base.DEC)
 opcode_number = ProtoField.int32("ccis.message","message", base.DEC)
+
 protocolName = ProtoField.string("ccis.protocolName","Protocol Name", base.NONE)
 headerVersion = ProtoField.string("ccis.headerVersion","Header Version", base.NONE)
 messageSequenceNumber = ProtoField.string("ccis.messageSequenceNumber","Message Sequence Number", base.NONE)
@@ -17,12 +18,24 @@ level4HeaderDataLength = ProtoField.string("ccis.level4HeaderDataLength","Level-
 level4HeaderVersion = ProtoField.string("ccis.level4HeaderVersion","Level-4 Header Version", base.NONE)
 ipVersion = ProtoField.string("ccis.ipVersion","ipVersion", base.NONE)
 
+localSelfIpAddress = ProtoField.string("ccis.localSelfIpAddress","localSelfIpAddress", base.NONE)
+localMateIpAddress = ProtoField.string("ccis.localMateIpAddress","localMateIpAddress", base.NONE)
+selfVirtualCircuitNumber = ProtoField.string("ccis.selfVirtualCircuitNumber","selfVirtualCircuitNumber", base.NONE)
+mateVirtualCircuitNumber = ProtoField.string("ccis.mateVirtualCircuitNumber","mateVirtualCircuitNumber", base.NONE)
+selfVirtualCicNumber = ProtoField.string("ccis.selfVirtualCicNumber","selfVirtualCicNumber", base.NONE)
+mateVirtualCicNumber = ProtoField.string("ccis.mateVirtualCicNumber","mateVirtualCicNumber", base.NONE)
+selfPortNumber = ProtoField.string("ccis.selfPortNumber","selfPortNumber", base.NONE)
+matePortNumber = ProtoField.string("ccis.matePortNumber","matePortNumber", base.NONE)
+
 
 ccis_protocol.fields = {
 	discriminator, message_length, from_ip, from_port, opcode_number,
 	protocolName, headerVersion, messageSequenceNumber, dataLengthOfLevel4Message,
 	cmdNumber,checksum,prisid,cktNumber,dtcLength,level4HeaderDataLength,
-	level4HeaderVersion,ipVersion
+	level4HeaderVersion,ipVersion,
+	localSelfIpAddress,localMateIpAddress,selfVirtualCircuitNumber,
+	mateVirtualCircuitNumber,selfVirtualCicNumber,mateVirtualCicNumber,
+	selfPortNumber,matePortNumber
 	}
 
 function ccis_protocol.dissector(buffer, pinfo, tree)
@@ -31,7 +44,7 @@ function ccis_protocol.dissector(buffer, pinfo, tree)
 
     pinfo.cols.protocol = ccis_protocol.name
     local mainTree = tree:add(ccis_protocol, buffer(), "NEC CCIS Protocol Data")
-    local ccisHeaderSubTree = mainTree:add("CCIS test Protocol Data")
+    local ccisHeaderSubTree = mainTree:add("CCIS Header")
 	ccisHeaderSubTree:add(protocolName, 				buffer(0,8))
 	ccisHeaderSubTree:add(headerVersion, 				buffer(8,2),  buffer(8,2):uint()  .. " -- (0x"..buffer(8,2) .."h)")
 	ccisHeaderSubTree:add(messageSequenceNumber, 		buffer(10,2), buffer(10,2):uint() .. " -- (0x"..buffer(10,2).."h)")
@@ -45,7 +58,22 @@ function ccis_protocol.dissector(buffer, pinfo, tree)
 	ccisHeaderSubTree:add(level4HeaderVersion, 			buffer(22,2), buffer(22,2):uint() .. " -- (0x"..buffer(22,2).."h)")
 	ccisHeaderSubTree:add(ipVersion, 					buffer(24,2), buffer(24,1):uint() .. " -- (0x"..buffer(24,1).."h)")
 
-	ccisHeaderSubTree:add(message_length, buffer(11,2))
+	local from_ip = bytes_to_ip(buffer,26)
+	local from_port = buffer(42,2):uint()
+	local to_ip = bytes_to_ip(buffer,30)
+	local to_port = buffer(44,2):uint()
+
+	local connectionInfo = mainTree:add("Connection Info")
+	connectionInfo:add(localSelfIpAddress, 		 buffer(26,4) , "From "..from_ip..":"..from_port)
+	connectionInfo:add(localMateIpAddress, 		 buffer(30,4), "To "..to_ip..":"..to_port)
+	connectionInfo:add(selfVirtualCircuitNumber, buffer(34,2), buffer(34,2):uint() .. " -- (0x"..buffer(34,2).."h)")
+	connectionInfo:add(mateVirtualCircuitNumber, buffer(36,2), buffer(36,2):uint() .. " -- (0x"..buffer(36,2).."h)")
+	connectionInfo:add(selfVirtualCicNumber, 	 buffer(38,2), buffer(38,2):uint() .. " -- (0x"..buffer(38,2).."h)")
+	connectionInfo:add(mateVirtualCicNumber, 	 buffer(40,2), buffer(40,2):uint() .. " -- (0x"..buffer(40,2).."h)")
+	connectionInfo:add(selfPortNumber, 			 buffer(42,2), buffer(42,2):uint() .. " -- (0x"..buffer(42,2).."h)")
+	connectionInfo:add(matePortNumber, 			 buffer(44,2), buffer(44,2):uint() .. " -- (0x"..buffer(44,2).."h)")
+
+	----ccisHeaderSubTree:add(message_length, buffer(11,2))
     if length>26 then
         local from_ip = bytes_to_ip(buffer,26)
 	local from_port = buffer(42,2):uint()
